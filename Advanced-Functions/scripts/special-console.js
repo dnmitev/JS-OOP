23fbb76fdac7c868a7a6d02af1154e04c4fde50a
Dimitar Mitev <dnmitev@gmail.com>
2014-06-29 17:55:59 +0300

    Classical oop hw is done

---
+++ Advanced-Functions/scripts/special-console.js
@@ -0,0 +1,56 @@
+﻿var specialConsole = (function () {
+    "use strict";
+    
+    function placeHolder(input) {
+        var format = input[0],
+            i;
+
+        for (i = 0; i < 100; i += 1) {
+            while (format.indexOf('{' + i + '}') !== -1) {
+                format = format.replace('{' + i + '}', input[i + 1]);
+            }
+        }
+        return format;
+    }
+
+    function writeLine(message) {
+        var argsCount = arguments.length;
+
+        if (argsCount === 1) {
+            console.log(message.toString());
+        } else {
+            var string = placeHolder(arguments);
+            console.log(string.toString());
+        }
+    }
+
+    function writeWarning(message) {
+        var argsCount = arguments.length,
+            string;
+
+        if (argsCount === 1) {
+            console.warn(message.toString());
+        } else {
+            string = placeHolder(arguments);
+            console.warn(string.toString());
+        }
+    }
+
+    function writeError(message) {
+        var argsCount = arguments.length,
+            string;
+
+        if (argsCount === 1) {
+            console.error(message.toString());
+        } else {
+            string = placeHolder(arguments);
+            console.error(string.toString());
+        }
+    }
+
+    return {
+        writeLine: writeLine,
+        writeWarning: writeWarning,
+        writeError: writeError
+    }
+}());
---
+++ Classical-OOP/scripts/draw-engine.js
@@ -0,0 +1,64 @@
+﻿var CanvasRenderEngine = function (canvasId) {
+    "use strict";
+
+    var canvas = document.querySelector(canvasId),
+        ctx = canvas.getContext('2d'),
+        drawLine,
+        drawCircle,
+        drawRect;
+
+    drawLine = function (x1, y1, x2, y2, color, width) {
+        var lineColor = color || 'black',
+            lineWidth = width || 1;
+
+        ctx.beginPath();
+
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+
+        ctx.strokeStyle = lineColor;
+        ctx.lineWidth = lineWidth;
+
+        ctx.stroke();
+    };
+
+    drawCircle = function (centerX, centerY, radius, fillColor, borderColor, borderWidth) {
+        var color = fillColor || 'white',
+            strokeColor = borderColor || 'black',
+            strokeWidht = borderWidth || 1;
+
+        ctx.beginPath();
+
+        ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+
+        ctx.strokeStyle = strokeColor;
+        ctx.lineWidth = strokeWidht;
+        ctx.fillStyle = color;
+
+        ctx.fill();
+        ctx.stroke();
+    };
+
+    drawRect = function (topX, topY, width, height, fillColor, borderColor, borderWidth) {
+        var color = fillColor || 'white',
+          strokeColor = borderColor || 'black',
+          strokeWidht = borderWidth || 1;
+
+        ctx.beginPath();
+
+        ctx.rect(topX, topY, width, height);
+
+        ctx.strokeStyle = strokeColor;
+        ctx.lineWidth = strokeWidht;
+        ctx.fillStyle = color;
+
+        ctx.fill();
+        ctx.stroke();
+    };
+
+    return {
+        drawLine: drawLine,
+        drawCicle: drawCircle,
+        drawRect: drawRect
+    };
+};
